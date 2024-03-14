4ea0616230b41b7ef66f837f7c30a3af14899f79
furkanyilmazPetroo <furkan.yilmaz@anttechbva.com>
2024-03-14 14:03:56 +0300

    private messaging screen is prepared

--- app/(tabs)/chats/[id].tsx
+++ app/(tabs)/chats/[id].tsx
@@ -1,12 +1,147 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import messageData from "@/assets/data/messages.json";
+import {
+  Bubble,
+  GiftedChat,
+  IMessage,
+  InputToolbar,
+  Send,
+  SystemMessage,
+} from "react-native-gifted-chat";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Page = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const insets = useSafeAreaInsets();
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    setMessages([
+      ...messageData.map((message) => {
+        return {
+          _id: message.id,
+          text: message.msg,
+          createdAt: new Date(message.date),
+          user: {
+            _id: message.from,
+            name: message.from ? "You" : "Bob",
+          },
+        };
+      }),
+      {
+        _id: 0,
+        system: true,
+        text: "All your base are belong to us",
+        createdAt: new Date(),
+        user: {
+          _id: 0,
+          name: "Bot",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ImageBackground
+      source={require("@/assets/images/pattern.png")}
+      style={{
+        flex: 1,
+        marginBottom: insets.bottom,
+        backgroundColor: Colors.background,
+      }}
+    >
+      <GiftedChat
+        messages={messages}
+        onSend={(messages: any) => onSend(messages)}
+        user={{ _id: 1 }}
+        renderSystemMessage={(props) => (
+          <SystemMessage {...props} textStyle={{ color: Colors.gray }} />
+        )}
+        onInputTextChanged={setText}
+        bottomOffset={insets.bottom}
+        renderAvatar={null}
+        maxComposerHeight={100}
+        renderBubble={(props) => {
+          return (
+            <Bubble
+              {...props}
+              textStyle={{ right: { color: "#000" } }}
+              wrapperStyle={{
+                left: { backgroundColor: "#fff" },
+                right: { backgroundColor: Colors.lightGreen },
+              }}
+            />
+          );
+        }}
+        renderSend={(props) => (
+          <View
+            style={{
+              flexDirection: "row",
+              height: 44,
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 14,
+              paddingHorizontal: 14,
+            }}
+          >
+            {text?.length > 0 && (
+              <Send containerStyle={{ justifyContent: "center" }} {...props} />
+            )}
+            {text?.length === 0 && (
+              <>
+                <Ionicons
+                  name="camera-outline"
+                  color={Colors.primary}
+                  size={28}
+                />
+                <Ionicons name="mic-outline" color={Colors.primary} size={28} />
+              </>
+            )}
+          </View>
+        )}
+        textInputProps={styles.composer}
+        renderInputToolbar={(props) => (
+          <InputToolbar
+            {...props}
+            containerStyle={{ backgroundColor: Colors.background }}
+            renderActions={() => (
+              <View
+                style={{
+                  height: 44,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  left: 5,
+                }}
+              >
+                <Ionicons name="add" color={Colors.primary} size={28} />
+              </View>
+            )}
+          />
+        )}
+      />
+    </ImageBackground>
   );
 };
 
+const styles = StyleSheet.create({
+  composer: {
+    backgroundColor: "#fff",
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: Colors.lightGray,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    marginVertical: 4,
+    paddingTop: 8,
+  },
+});
 export default Page;
